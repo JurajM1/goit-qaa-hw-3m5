708f264ba325a8dd58344857e3db79ee5c53a892
Juraj Marcinko <jurajmar@yahoo.ie>
2024-05-26 17:42:05 +0200

    uzycie "map"

--- hw3m5map.js
+++ hw3m5map.js
@@ -11,31 +11,31 @@ function myColorChange() {
 // Zadanie czesc 1 tablice podwojona liczby
 // Funkcja podwajająca liczby w tablicy za pomocą map()
 
-// const doubleNumbers = (numbers) => {
-//   return numbers.map(number => number * 2);
-// };
+const doubleNumbers = (numbers) => {
+  return numbers.map(number => number * 2);
+};
 
 // Testowanie funkcji doubleNumbers
 
-// const numbersArray = [1, 2, 3, 4, 5];
-// console.log("Oryginalna tablica liczb:", numbersArray);
-// console.log("Tablica liczb podwojonych:", doubleNumbers(numbersArray));
+const numbersArray = [1, 2, 3, 4, 5];
+console.log("Oryginalna tablica liczb:", numbersArray);
+console.log("Tablica liczb podwojonych:", doubleNumbers(numbersArray));
 
 
 //------------------------------------------------------------------------
 // Zadanie czesc 2 Tablice imion
 // Funkcja zwracająca tablicę imion studentów z tablicy obiektów
 
-// const getStudentNames = (students) => {
-//   return students.map(student => student.name);
-// };
+const getStudentNames = (students) => {
+  return students.map(student => student.name);
+};
 
 // Testowanie funkcji getStudentNames
-// const studentsArray = [
-//   { id: 1, name: "Tom" },
-//   { id: 2, name: "Klara" },
-//   { id: 3, name: "Juraj" },
-//   { id: 4, name: "Zenek" }
-// ];
-// console.log("Oryginalna tablica obiektów studentów:", studentsArray);
-// console.log("Tablica imion studentów:", getStudentNames(studentsArray));
+const studentsArray = [
+  { id: 1, name: "Tom" },
+  { id: 2, name: "Klara" },
+  { id: 3, name: "Juraj" },
+  { id: 4, name: "Zenek" }
+];
+console.log("Oryginalna tablica obiektów studentów:", studentsArray);
+console.log("Tablica imion studentów:", getStudentNames(studentsArray));
